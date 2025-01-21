41e98b87c8ec524856e121cc8a86a969e66dabe4
KPS\gyeongu.kang <yoshizukiyo@gmail.com>
2025-01-21 10:14:04 +0900

    feat - 포트폴리오 업데이트

--- portfolio/Responsive web/js/custom_3.js
+++ portfolio/Responsive web/js/custom_3.js
@@ -1,45 +1,37 @@
+$(function () {
+	// 스크롤 이벤트
+	AOS.init();
 
-$(function(){
+	$('.m_vis_btn').click(function () {
+		$('.aside').addClass('on');
+	});
 
-  // 스크롤 이벤트
-  AOS.init();
+	$('.close').click(function () {
+		$('.aside').removeClass('on');
+	});
 
-  
-  $('.m_vis_btn').click(function(){
-    $('.aside').addClass('on');
-  });
-  
-  $('.close').click(function(){
-    $('.aside').removeClass('on');
-  });
+	$('.menu a').click(function () {
+		t = $(this).data('target');
+		h = $('#' + t).offset().top;
+		$('html,body').animate({ scrollTop: h }, 300);
+	});
 
+	$('.aside li a').click(function () {
+		t = $(this).data('target');
+		h = $('#' + t).offset().top;
+		$('html,body').animate({ scrollTop: h }, 300);
+		$('.aside').removeClass('on');
+	});
 
-  $('.menu a').click(function(){
-    t = $(this).data('target')
-    h = $('#'+t).offset().top;
-    $('html,body').animate({scrollTop:h},300)
-  });
+	new Swiper('.swiper-container', {
+		loop: true,
+		slidesPerView: 'auto',
+		centeredSlides: true,
+		spaceBetween: 30,
 
-  $('.aside li a').click(function(){
-    t = $(this).data('target')
-    h = $('#'+t).offset().top;
-    $('html,body').animate({scrollTop:h},300)
-    $('.aside').removeClass('on');
-  });
-
-  var swiper = new Swiper('.swiper-container', {
-    loop:true,
-    slidesPerView: 'auto',
-    centeredSlides: true,
-    spaceBetween: 30,
-  
-    // autoplay: {
-    //   delay: 3000,
-    //   disableOnInteraction: false,
-    // }
-  });
-  
-
-
-
+		autoplay: {
+			delay: 3000,
+			disableOnInteraction: false,
+		},
+	});
 }); //지우지 마세요
